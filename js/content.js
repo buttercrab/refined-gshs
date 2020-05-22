d680fce6eeb705c1caa86d1a4e853eefc355f3ae
gs18113 <seungmin960@gmail.com>
2020-05-22 11:46:31 +0900

    autologin in all pages

--- js/content.js
+++ js/content.js
@@ -1,7 +1,9 @@
-chrome.storage.local.get(["login"], function(items){
-    items = items['login'];
-    if(items.userId === undefined || items.userPw === undefined) return;
-    document.getElementById("tempUserId").value = items.userId;
-    document.getElementById("tempPwd").value = items.userPw;
-    document.getElementsByClassName("btn btn-primary btn-flat pull-right")[0].click();
-});
+if(document.body.classList.contains('register-page')){
+    chrome.storage.local.get(["login"], function(items){
+        items = items['login'];
+        if(items.userId === undefined || items.userPw === undefined) return;
+        document.getElementById("tempUserId").value = items.userId;
+        document.getElementById("tempPwd").value = items.userPw;
+        document.getElementsByClassName("btn btn-primary btn-flat pull-right")[0].click();
+    });
+}
